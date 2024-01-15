e05ffcb250ca0f11b2394ba4b12415be2d46054c
cameron <cmrnwlsh@gmail.com>
2024-01-15 04:47:41 -0500

    terminal view mostly complete

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,14 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         let leptos_options = &conf.leptos_options;
-        let site_root = &leptos_options.site_root;
 
         App::new()
             .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
             .route("/favicon.ico", web::get().to(|| async {
                 NamedFile::open("./target/site/favicon.ico")
             }))
-            .service(Files::new("/pkg", format!("{site_root}/pkg")))
-            .service(Files::new("/assets", site_root))
+            .service(Files::new("/pkg", "target/site/pkg"))
+            .service(Files::new("/assets", "target/site"))
             .leptos_routes(leptos_options.to_owned(), routes.to_owned(), App)
             .app_data(web::Data::new(leptos_options.to_owned()))
             .wrap(middleware::Compress::default())
