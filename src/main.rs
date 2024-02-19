5de9c9d6e8752a818301c8602054c22a6c4f6b0c
cameron <cmrnwlsh@gmail.com>
2024-02-19 10:56:04 -0500

    enable hsts

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use actix_files::NamedFile;
 use actix_files::Files;
+use actix_web::body::MessageBody;
+use actix_web::dev::ServiceResponse;
 use actix_web::*;
+use actix_http::header::TryIntoHeaderValue;
 use leptos::*;
 use leptos_actix::{generate_route_list, LeptosRoutes};
 use website::app::*;
 use openssl::ssl::{SslAcceptor, SslMethod, SslFiletype};
-use actix_web_lab::{header::StrictTransportSecurity, middleware::RedirectHttps};
+use actix_web_lab::{header::StrictTransportSecurity, middleware::map_response};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -22,7 +25,15 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         let leptos_options = &conf.leptos_options;
-        let hsts =RedirectHttps::with_hsts(StrictTransportSecurity::recommended());
+        async fn hsts_header(
+            mut res: ServiceResponse<impl MessageBody>,
+        ) -> Result<ServiceResponse<impl MessageBody>, Error> {
+            res.headers_mut()
+                .insert(http::header::STRICT_TRANSPORT_SECURITY, 
+                    StrictTransportSecurity::recommended().preload().try_into_value()?);
+
+            Ok(res)
+        }
         App::new()
             .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
             .route(
@@ -35,7 +46,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(leptos_options.to_owned()))
             .wrap(middleware::Compress::default())
             .wrap(middleware::Logger::default())
-            .wrap(hsts)
+            .wrap(map_response(hsts_header))
     })
     .bind_openssl(&addr, ssl_builder)?
     .run()
