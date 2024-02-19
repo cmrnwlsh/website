9f6cd385f35e689028a89aeecfcd7c91c8af9e0e
cameron <cmrnwlsh@gmail.com>
2024-02-19 11:55:23 -0500

    enable hsts

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use actix_web_lab::{header::StrictTransportSecurity, middleware::map_response};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let conf = get_configuration(None).await.unwrap();
-    let addr = conf.leptos_options.site_addr;
+    let addr = "0.0.0.0:443";
     let routes = generate_route_list(App);
     let mut ssl_builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
         ssl_builder
