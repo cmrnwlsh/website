07b178e8f01e2623f49342f85e8c6c8787366772
cameron <cmrnwlsh@gmail.com>
2024-05-01 04:42:33 -0400

    changing scss variablies to regular css

--- src/app/term.rs
+++ src/app/term.rs
@@ -5,10 +5,8 @@ use leptos::*;
 #[component]
 pub fn Term() -> impl IntoView {
     let input_ref: NodeRef<Input> = create_node_ref();
-    let (output, set_output) =
-        create_signal(vec![String::from("Hello world")]);
-    let (input, set_input) =
-        create_signal(String::from(""));
+    let (output, set_output) = create_signal(vec![String::from("Hello world")]);
+    let (input, set_input) = create_signal(String::from(""));
 
     view! {
         <div id="term">
@@ -18,7 +16,7 @@ pub fn Term() -> impl IntoView {
                     set_output.update(|v| v.push(input_ref().unwrap().value()));
                     set_input("".into())
                 }>
-                    <label for="input">~></label>
+                    <label for="input">{"~>"}</label>
                     <input type="text" id="input" node_ref=input_ref prop:value=input />
                 </form>
             </div>
